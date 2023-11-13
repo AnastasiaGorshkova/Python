a6839a644e762df22ba196c645311d153b74e810
AnastasiaGorshkova <114027000+AnastasiaGorshkova@users.noreply.github.com>
2023-11-13 20:45:26 +0300

    Add files via upload

---
+++ Lab_1/Сorrections/Ex 2/2.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+# Читаем файл
+with open('2.txt', 'r') as file:
+    lines = file.readlines()
+
+    # разбиваем на координаты X и Y
+xs = []
+ys = []
+for i in range(0, len(lines), 2):
+    xs.append(list(map(float, lines[i].split())))
+    ys.append(list(map(float, lines[i + 1].split())))
+
+for i in range(len(xs)):
+    plt.figure()
+    plt.plot(xs[i], ys[i])
+    plt.xlim(0, 16)  # вот здесь расширила область оси вправо
+    plt.ylim(-10, 12)  # аот здесь уверилила верхний предел оси
+    plt.title(f'График {i+1}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(True)
+    plt.savefig(f'График_{i+1}.png')
+    plt.show()
+
