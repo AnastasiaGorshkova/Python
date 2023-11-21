3cf4e9b0ce53e9085ef71e7b45ceeced18ea76d9
AnastasiaGorshkova <114027000+AnastasiaGorshkova@users.noreply.github.com>
2023-11-21 17:37:29 +0300

    Update Task_2.py

--- Lab_3/Task_2.py
+++ Lab_3/Task_2.py
@@ -4,10 +4,18 @@ import matplotlib.pyplot as plt
 
 for path in os.listdir('./signals'):
     with open('./signals/' + path) as file:
-        initial = np.array(list(map(float, file.readlines())))  # массив исходных данных
-        treated = np.array([np.mean(initial[max(0, i - 9) : i + 1]) for i in range(np.size(initial))])
+        # initial = np.array(list(map(float, file.readlines())))  # массив исходных данных
+        initial = np.loadtxt(file, dtype=float)
+
+        # treated = np.array([np.mean(initial[max(0, i - 9): i + 1]) for i in range(np.size(initial))])
 # массив обработанных данных: каждый элемент = среднее значение 10 предыдущих элементов (если они существуют)
 # и текущего элемента массива исходных данных.
+
+        base = np.ones(10) / 10
+        treated = np.convolve(initial, base, mode='same')
+# Создание окна с помощью np.ones(10) / 10 = массив из 10 элементов, каждый из которых равен 1/10.
+# Функция np.convolve() = применение этого окна к массиву initial.
+# Параметр mode='same' = размер итогового массива такой же, как и у исходного массива.
 
         fig, ax = plt.subplots()
         ax.plot(initial, linewidth=1, label='initial', color='#4682B4')
